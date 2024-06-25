4708aa562a2d2d6e6c949bcebd1c696523c97fa6
DrBrad <brad@bradeagle.com>
2024-06-25 16:36:30 -0600

    Bencode system working

--- src/filesystem/kfs.rs
+++ src/filesystem/kfs.rs
@@ -1,103 +1,135 @@
 use std::ffi::OsStr;
 use std::time::{Duration, UNIX_EPOCH};
+use bencode::variables::bencode_array::{AddArray, BencodeArray};
 use fuser::{FileAttr, Filesystem, FileType, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry, Request};
 
 const TTL: Duration = Duration::from_secs(1); // 1 second
 
-const HELLO_DIR_ATTR: FileAttr = FileAttr {
-    ino: 1,
-    size: 0,
-    blocks: 0,
-    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
-    mtime: UNIX_EPOCH,
-    ctime: UNIX_EPOCH,
-    crtime: UNIX_EPOCH,
-    kind: FileType::Directory,
-    perm: 0o755,
-    nlink: 2,
-    uid: 501,
-    gid: 20,
-    rdev: 0,
-    flags: 0,
-    blksize: 512,
-};
+pub struct KFS {
+    files: BencodeArray
+}
 
-const HELLO_DIR_ATTR2: FileAttr = FileAttr {
-    ino: 3, // Replace with appropriate inode number
-    size: 0, // Directories generally have a size of 0
-    blocks: 0,
-    atime: UNIX_EPOCH, // Last access time (Unix epoch for simplicity)
-    mtime: UNIX_EPOCH, // Last modification time (Unix epoch for simplicity)
-    ctime: UNIX_EPOCH, // Last status change time (Unix epoch for simplicity)
-    crtime: UNIX_EPOCH, // Creation time (Unix epoch for simplicity)
-    kind: FileType::Directory, // Indicates it's a directory
-    perm: 0o755, // Permissions: rwxr-xr-x (Owner: Read, Write, Execute; Group: Read, Execute; Others: Read, Execute)
-    nlink: 2, // Number of hard links (typically 2 for a directory)
-    uid: 501, // Replace with actual user ID
-    gid: 20, // Replace with actual group ID
-    rdev: 0,
-    flags: 0,
-    blksize: 512, // Block size for filesystem I/O operations
-};
+impl KFS {
 
-const HELLO_TXT_CONTENT: &str = "Hello World!\n";
-
-const HELLO_TXT_ATTR: FileAttr = FileAttr {
-    ino: 2,
-    size: 13,
-    blocks: 1,
-    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
-    mtime: UNIX_EPOCH,
-    ctime: UNIX_EPOCH,
-    crtime: UNIX_EPOCH,
-    kind: FileType::RegularFile,
-    perm: 0o644,
-    nlink: 1,
-    uid: 501,
-    gid: 20,
-    rdev: 0,
-    flags: 0,
-    blksize: 512,
-};
-
-pub struct KFS;
+    pub fn new(mut files: BencodeArray) -> Self {
+        Self {
+            files
+        }
+    }
+}
 
 impl Filesystem for KFS {
 
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
         if parent == 1 {
-            match name.to_str() {
-                Some("hello.txt") => reply.entry(&TTL, &HELLO_TXT_ATTR, 0),
-                Some("test") => reply.entry(&TTL, &HELLO_DIR_ATTR2, 0),
-                _ => reply.error(2), // Return error if name is not recognized
+            println!("{}", name.to_str().unwrap());
+
+            for i in 0..self.files.size() {
+                if self.files.get_object(i).unwrap().get_string("name").unwrap() == name.to_str().unwrap() {
+                    match self.files.get_object(i).unwrap().get_string("type").unwrap() {
+                        "file" => {
+                            reply.entry(&TTL, &FileAttr {
+                                ino: (i as u64)+2,
+                                size: self.files.get_object(i).unwrap().get_number("size").unwrap(),
+                                blocks: 1,
+                                atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+                                mtime: UNIX_EPOCH,
+                                ctime: UNIX_EPOCH,
+                                crtime: UNIX_EPOCH,
+                                kind: FileType::RegularFile,
+                                perm: 0o777,
+                                nlink: 1,
+                                uid: 501,
+                                gid: 20,
+                                rdev: 0,
+                                flags: 0,
+                                blksize: 512
+                            }, 0);
+                        },
+                        "directory" => {
+                            reply.entry(&TTL, &FileAttr {
+                                ino: (i as u64)+2,
+                                size: 0,
+                                blocks: 0,
+                                atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+                                mtime: UNIX_EPOCH,
+                                ctime: UNIX_EPOCH,
+                                crtime: UNIX_EPOCH,
+                                kind: FileType::Directory,
+                                perm: 0o755,
+                                nlink: 2,
+                                uid: 501,
+                                gid: 20,
+                                rdev: 0,
+                                flags: 0,
+                                blksize: 512
+                            }, 0);
+                        }
+                        _ => reply.error(2)
+                    }
+                    return;
+                }
             }
-        } else {
-            reply.error(2); // Return error for unknown parent directory
         }
-        /*
-        if parent == 1 && name.to_str() == Some("hello.txt") {
-            reply.entry(&TTL, &HELLO_TXT_ATTR, 0);
-        } else {
-            reply.error(2);
-        }
-        */
+
+        reply.error(2); // Return error for unknown parent directory
     }
 
     fn getattr(&mut self, _req: &Request<'_>, ino: u64, reply: ReplyAttr) {
-        match ino {
-            1 => reply.attr(&TTL, &HELLO_DIR_ATTR),
-            3 => reply.attr(&TTL, &HELLO_DIR_ATTR2),
-            2 => reply.attr(&TTL, &HELLO_TXT_ATTR),
-            _ => reply.error(2),
+        println!("{}", ino);
+
+        if ino == 1 {
+            reply.attr(&TTL, &FileAttr {
+                ino: 1,
+                size: 0,
+                blocks: 0,
+                atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+                mtime: UNIX_EPOCH,
+                ctime: UNIX_EPOCH,
+                crtime: UNIX_EPOCH,
+                kind: FileType::Directory,
+                perm: 0o755,
+                nlink: 2,
+                uid: 501,
+                gid: 20,
+                rdev: 0,
+                flags: 0,
+                blksize: 512
+            });
+            return;
+        }
+
+        let file = self.files.get_object((ino as usize)-2).unwrap();
+        match file.get_string("type").unwrap() {
+            "file" => {
+                reply.attr(&TTL, &FileAttr {
+                    ino,
+                    size: file.get_number("size").unwrap(),
+                    blocks: 1,
+                    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+                    mtime: UNIX_EPOCH,
+                    ctime: UNIX_EPOCH,
+                    crtime: UNIX_EPOCH,
+                    kind: FileType::RegularFile,
+                    perm: 0o777,
+                    nlink: 1,
+                    uid: 501,
+                    gid: 20,
+                    rdev: 0,
+                    flags: 0,
+                    blksize: 512
+                });
+            },
+            _ => reply.error(2)
         }
     }
 
     fn read(&mut self, _req: &Request, ino: u64, _fh: u64, offset: i64, _size: u32, _flags: i32, _lock: Option<u64>, reply: ReplyData) {
-        if ino == 2 {
-            reply.data(&HELLO_TXT_CONTENT.as_bytes()[offset as usize..]);
-        } else {
+        //if ino == 2 {
+            //reply.data(&HELLO_TXT_CONTENT.as_bytes()[offset as usize..]);
+        //} else {
             reply.error(2);
-        }
+        //}
     }
 
     fn readdir(&mut self, _req: &Request, ino: u64, _fh: u64, offset: i64, mut reply: ReplyDirectory) {
@@ -106,27 +138,28 @@ impl Filesystem for KFS {
             return;
         }
 
-        let entries = vec![
-            (1, FileType::Directory, "."),
-            (1, FileType::Directory, ".."),
-            (3, FileType::Directory, "test"),
-            (2, FileType::RegularFile, "hello.txt"),
-        ];
+        let default = [ ".", ".." ];
 
-        for (i, &(inode, file_type, name)) in entries.iter().enumerate().skip(offset as usize) {
-            if reply.add(inode, (i + 1) as i64, file_type, name) {
-                break;
+        for i in (offset as usize)..self.files.size()+2 {
+            if i < 2 {
+                reply.add(1, (i as i64)+1, FileType::Directory, default[i]);
+                continue;
+            }
+
+            match self.files.get_object(i-2).unwrap().get_string("type").unwrap() {
+                "file" => {
+                    reply.add(i as u64, (i as i64)+1, FileType::RegularFile, self.files.get_object(i-2).unwrap().get_string("name").unwrap());
+                },
+                "directory" => {
+                    reply.add(i as u64, (i as i64)+1, FileType::Directory, self.files.get_object(i-2).unwrap().get_string("name").unwrap());
+                },
+                _ => {
+                    reply.error(2);
+                    return;
+                }
             }
         }
 
-        /*
-        for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
-            // i + 1 means the index of the next entry
-            if reply.add(entry.0, (i + 1) as i64, entry.1, entry.2) {
-                break;
-            }
-        }
-        */
         reply.ok();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ pub mod filesystem;
 
 use bencode::variables::bencode_array::{AddArray, BencodeArray};
 use bencode::variables::bencode_object::{BencodeObject, PutObject};
+use bencode::variables::inter::bencode_variable::BencodeVariable;
 use filesystem::kfs::KFS;
 use fuser::MountOption;
 
 fn main() {
     let mut ben = BencodeArray::new();
+
     let mut file = BencodeObject::new();
     file.put("name", "hello_world.txt");
     file.put("type", "file");
@@ -19,7 +21,8 @@ fn main() {
     dir.put("type", "directory");
     ben.add(dir);
 
-    let mountpoint = "/media/test";
+
+    let mountpoint = "/media/test2";
     let mut options = vec![MountOption::RO, MountOption::FSName("KFS".to_string())];
-    fuser::mount2(KFS, mountpoint, &options).unwrap();
+    fuser::mount2(KFS::new(ben), mountpoint, &options).unwrap();
 }
