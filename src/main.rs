f9fa65f4499526f3b60ad0f1edcd1758dbfbfc3a
DrBrad <brad@bradeagle.com>
2024-06-25 14:49:32 -0600

    Bencode filesystem handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
+extern crate bencode;
 pub mod filesystem;
+
+use bencode::variables::bencode_array::{AddArray, BencodeArray};
+use bencode::variables::bencode_object::{BencodeObject, PutObject};
 use filesystem::kfs::KFS;
 use fuser::MountOption;
 
 fn main() {
+    let mut ben = BencodeArray::new();
+    let mut file = BencodeObject::new();
+    file.put("name", "hello_world.txt");
+    file.put("type", "file");
+    file.put("size", 14);
+    ben.add(file);
+
+    let mut dir = BencodeObject::new();
+    dir.put("name", "test");
+    dir.put("type", "directory");
+    ben.add(dir);
+
     let mountpoint = "/media/test";
     let mut options = vec![MountOption::RO, MountOption::FSName("KFS".to_string())];
     fuser::mount2(KFS, mountpoint, &options).unwrap();
