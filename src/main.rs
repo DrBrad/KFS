6f69bfaf5547918b9f25c1ba5e6e54c610ce68cb
DrBrad <brad@bradeagle.com>
2024-06-25 14:21:12 -0600

    FileSystem changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-pub mod kfs;
+pub mod filesystem;
+use filesystem::kfs::KFS;
 use fuser::MountOption;
 
 fn main() {
     let mountpoint = "/media/test";
     let mut options = vec![MountOption::RO, MountOption::FSName("KFS".to_string())];
-    fuser::mount2(kfs::KFS, mountpoint, &options).unwrap();
+    fuser::mount2(KFS, mountpoint, &options).unwrap();
 }
