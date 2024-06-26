1d67b856d3fe6d32b58e874ebc871bc2dfb90618
DrBrad <brad@bradeagle.com>
2024-06-26 11:03:32 -0600

    Make Directories

--- src/filesystem/kdirectory.rs
+++ src/filesystem/kdirectory.rs
@@ -2,15 +2,35 @@ use fuser::FileType;
 use crate::filesystem::inter::file::File;
 
 pub struct KDirectory {
-    name: String
+    name: String,
+    files: Vec<Box<dyn File>>
 }
 
 impl KDirectory {
 
     pub fn new(name: &str) -> Self {
         Self {
-            name: name.to_string()
+            name: name.to_string(),
+            files: Vec::new()
         }
+    }
+
+    /*
+    pub fn get_files(&self) -> Vec<Box<dyn File>> {
+        self.files.clone()
+    }
+    */
+
+    pub fn get_file(&self, index: usize) -> &Box<dyn File> {
+        self.files.get(index).unwrap()
+    }
+
+    pub fn add_file(&mut self, file: &dyn File) {
+        self.files.push(file.clone_dyn());
+    }
+
+    pub fn len(&self) -> usize {
+        self.files.len()
     }
 }
 
