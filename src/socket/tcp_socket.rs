c925cc5a17c782c4b5c7c31d3fa50cf7d3307ce1
DrBrad <brad@bradeagle.com>
2024-07-08 15:34:58 -0600

    TCP Socket

---
+++ src/socket/tcp_socket.rs
@@ -0,0 +1,25 @@
+use crate::socket::socket::Socket;
+
+pub struct TcpSocket {
+
+}
+
+impl Socket for TcpSocket {
+
+    fn handshake(&self) {
+        todo!()
+    }
+
+    fn get_list(&self) {
+        todo!()
+    }
+
+    fn write(&self) {
+        todo!()
+    }
+
+    fn read(&self) {
+        todo!()
+    }
+}
+
